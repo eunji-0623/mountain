596fb380c4332a241fe757c7c94f82e1d61f7c93
Eunji Hwang <dmswl0489@gmail.com>
2025-10-09 13:35:21 +0900

    feat: list 페이지 필터 추가

--- app/pages/list/index.vue
+++ app/pages/list/index.vue
@@ -5,15 +5,37 @@ import { useMountainStore } from '~/stores/mountain';
 const { getMountainInfo } = storeToRefs(useMountainStore());
 const mountainInfo = getMountainInfo.value;
 
+const complete = ref('all');
+const sort = ref('name');
+
 definePageMeta({
   layout: 'common',
 });
 </script>
 
 <template>
+  <div class="filter-wrap">
+    <div class="filter">
+      <p>완료여부</p>
+      <select v-model="complete">
+        <option value="all">전체</option>
+        <option value="completed">완료</option>
+        <option value="notCompleted">미완료</option>
+      </select>
+    </div>
+    <div class="filter">
+      <p>정렬</p>
+      <select v-model="sort">
+        <option value="name">이름순</option>
+        <option value="difficulty">난이도순</option>
+      </select>
+    </div>
+  </div>
   <div class="card-container">
     <Card v-for="mountain in mountainInfo" :key="mountain.id" :mountain-info="mountain" />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@import '~/assets/card.scss';
+</style>
