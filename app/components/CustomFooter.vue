2bd80564f36eff08449c50dfdc865e3bde067261
Eunji Hwang <dmswl0489@gmail.com>
2025-10-24 16:26:46 +0900

    feat: footer 아이콘 변경 (작업중)

--- app/components/CustomFooter.vue
+++ app/components/CustomFooter.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+import calendar from '~/assets/images/icon_calendar.png';
+import calendarActive from '~/assets/images/icon_calendar_active.png';
+import list from '~/assets/images/icon_list.png';
+import listActive from '~/assets/images/icon_list_active.png';
+
+const route = useRoute();
+const isCalendar = computed(() => route.path?.includes('calendar'));
+const isList = computed(() => route.path?.includes('list'));
+const isMap = computed(() => route.path?.includes('map'));
+const isMenu = computed(() => route.path?.includes('menu'));
+
 function handleCalendarClick() {
   navigateTo('/calendar');
 }
@@ -19,9 +30,13 @@ function handleMyMenuClick() {
 <template>
   <footer>
     <!--  캘린더   -->
-    <button @click="handleCalendarClick">CAL</button>
+    <button @click="handleCalendarClick">
+      <img :src="isCalendar ? calendarActive : calendar" alt="달력" />
+    </button>
     <!--  산 (목록)  -->
-    <button @click="handleListClick">LIST</button>
+    <button @click="handleListClick">
+      <img :src="isList ? listActive : list" alt="목록" />
+    </button>
     <!--  산 (지도)  -->
     <button @click="handleMapClick">MAP</button>
     <!--  my menu  -->
